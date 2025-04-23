4b205cf497241934c8c472e60b586954a836a944
aylahauser <adhauser@smith.edu>
2025-04-23 09:15:12 -0700

    started writing my functions

--- R/plottie.R
+++ R/plottie.R
@@ -1,8 +1,10 @@
-
-
-
-
-easy_boxplot <- function(dataset, x, y, color, fill, ...){
+#' @title Create a Boxplot
+#' @description
+#' Uses inserted variables and data set to create a boxplot that will be displayed in the RStudio plot window.
+#'
+#' @importFrom ggplot2 ggplot geom_boxplot theme_minimal
+#' @export
+easy_boxplot <- function(dataset, x, y, color, ...){
 
   independent <- dataset$x
   dependent <- dataset$y
@@ -10,9 +12,31 @@ easy_boxplot <- function(dataset, x, y, color, fill, ...){
   if (is.character(dependent) == TRUE | is.logical(dependent) == TRUE){
     stop("A boxplot uses a continuous variable on the Y-axis.")
   }
-
-  boxplot <- ggplot(data = dataset,
+  boxplot <- ggplot2::ggplot(data = dataset,
                     mapping = aes(x = independent,
-                                  y = dependent)) +
-    geom_boxplot()
+                                  y = dependent,
+                                  color = color)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::theme_minimal()
+  return(boxplot)
 }
+
+easy_lineplot <- function(dataset, x, y, color, ...){
+
+  independent <- dataset$x
+  dependent <- dataset$y
+
+  if (is.character(dependent) == TRUE | is.logical(dependent) == TRUE){
+    stop("A lineplot uses a continuous variable on the Y-axis.")
+  }
+  lineplot <- ggplot2::ggplot(data = dataset,
+                             mapping = aes(x = independent,
+                                           y = dependent,
+                                           color = color)) +
+    ggplot2::geom_line() +
+    ggplot2::theme_minimal()
+  return(lineplot)
+}
+
+
+
