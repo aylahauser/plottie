4cb368dd482792b4270e6771a044a11c570d4808
Melody Mei <ymei@smith.edu>
2025-05-07 22:25:56 -0400

    Fixed some errors in the unit test

--- tests/testthat/test-easy_boxplot.R
+++ tests/testthat/test-easy_boxplot.R
@@ -1,7 +1,8 @@
 test_that("easy_boxplot returns a ggplot object", {
-  plot <- easy_boxplot(mtcars, x = "cyl", y = "mpg", color = "gear")
+  plot <- easy_boxplot(iris, x = "Species", y = "Sepal.Length")
   expect_s3_class(plot, "ggplot")
 })
+
 
 test_that("easy_boxplot errors when y is not numeric", {
   expect_error(easy_boxplot(iris, x = "Species", y = "Species"))
